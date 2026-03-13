604feb4451ed3fac1ced9c57fefbdab5667379ff
Bianca Moura <biancamouradev@gmail.com>
2026-03-12 23:19:23 -0300

    Perfil do usuário

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const perfilRoutes = require("./routes/perfilRoutes");
 
 const app = express();
+
 
 app.use(express.json());
 
 // Rotas da API (prefixo /api)
 app.use("/api", userRoutes);
+app.use("/api", perfilRoutes);
 // Outros alunos: adicionar aqui as novas rotas, ex.:
 // app.use("/api", perfilRoutes);
 // app.use("/api", categoriaRoutes);
