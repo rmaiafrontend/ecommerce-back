2fa554c9d137aeed4beee6ad021f652ce6a6b751
rmaiafrontend <rmaiafrontend@gmail.com>
2026-03-14 14:53:12 -0300

    fix: remove to-do-list fails

--- to-do-list/models/Person.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const personSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    age: {type: Number},
-    profile: {type: mongoose.Schema.Types.ObjectId, ref:"Profile"}
-});
-module.exports = mongoose.model("Person", personSchema)
