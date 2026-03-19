d8d7cd711b5ed9a64f0db87902a22bab5ef28ce2
V1perzin <jose.teles@maisunifacisa.com.br>
2026-03-19 14:33:21 -0300

    Adicionado produto as Tags.js

--- ecommerce/models/Tag.js
+++ ecommerce/models/Tag.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
 const tagSchema = new mongoose.Schema({
-    nome: {type: String}
+    nome: {type: String},
+    produto: [{ type: mongoose.Schema.Types.ObjectId, ref: "Produto" }]
 });
 
 module.exports = mongoose.model("Tag", tagSchema)
